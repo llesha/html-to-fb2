f78935035e197c35679e3055876b70674976472e
Alex5041 <kononal@gmail.com>
2023-03-29 19:23:13 +0300

    update README.md

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Translate html blogs to fb2 books
 # How to use
 
 ### JVM
-type `java -jar fb2_to_html.jar <BLOG_PATH> <ADD_IMAGES> <ADD_LINKS>`
+type `java -jar fb2_to_html.jar <PARAMS>
 ### JS (Browser)
 (Currently only JVM version works, browser will be added in future)
-### Arguments
-* `BLOG_PATH` - path to the blog page
-* `ADD_IMAGES` - true to export images
-* `ADD_LINKS` - true to add pages on the same site visitable from current page and all other recursively.
+
+# Problems
+
+Images, tables are not supported
--- src/commonTest/kotlin/XmlParserTest.kt
+++ src/commonTest/kotlin/XmlParserTest.kt
@@ -15,39 +15,4 @@ class XmlParserTest {
                     """
         XmlParser().parse(text)
     }
-
-    @Test
-    fun parseFragment() {
-        val text = """<p><strong>Keep salaries secret</strong>. 
-            It’s obvious: Don’t let them discuss 
-            <a href="/2015/07/21/hourly-pay-modern-slavery.html">salaries</a>
-            . They must keep this information secret. Warn them or even sign 
-            <a href="/2015/05/04/how-to-protect-business-idea.html">NDAs</a> 
-            prohibiting any talks about wages, bonuses, compensation plans, etc. 
-            They must feel that this information is toxic and 
-            never even talk to each other about salaries. 
-            If they don’t know how much their coworkers are getting, 
-            they won’t raise salary questions for much longer.</p>"""
-
-        val (_, el) = XmlParser().parse(text)
-        val tag = TagFactory().createTag(el)
-        println(tag.getFb2(HtmlTranslator("", WriteStream("C:\\Users\\alex\\Desktop\\book.fb2"))))
-    }
-
-    @Test
-    fun parseFragmentWithUnclosedTag() {
-        val text = """
-            <p><img src="https://i.ytimg.com/vi/PJAzfQtxby4/mqdefault.jpg" 
-            class="past-talk" alt="code/R 2021"> 
-            Анализ качества кода и почему он не работает?; 
-            <a href="https://code-r.ru/" class="foreign">&lt;code/R&gt;</a>; 
-            <span class="city">Omsk</span>, <span class="country">Russia</span>; 
-            18 September; <a href="https://www.youtube.com/watch?v=PJAzfQtxby4" 
-            class="foreign">видео</a>.</p>
-        """
-        val (_, el) = XmlParser().parse(text)
-        val tag = TagFactory().createTag(el)
-        println(tag.getFb2(HtmlTranslator("", WriteStream("C:\\Users\\alex\\Desktop\\book.fb2"))))
-        println(el)
-    }
 }
